66ccd9533d463c994b7617265804edf66c0629ae
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2024-12-05 16:57:56 -0500

    feat: rol insert

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,6 @@ INSERT INTO master_details (master_id, description) VALUES (1, 'Colombia');
 INSERT INTO masters (description, status) VALUES ('Currency', true);
 INSERT INTO master_details (master_id, description) VALUES (2, 'Soles');
 INSERT INTO master_details (master_id, description) VALUES (2, 'Dolares');
+
+INSERT INTO roles (description, created_at, updated_at, status) VALUES ('Administrator', '2024-12-05 16:52:45.436', '2024-12-05 16:52:45.436', true);
+INSERT INTO roles (description, created_at, updated_at, status) VALUES ('Technical', '2024-12-05 16:52:45.436', '2024-12-05 16:52:45.436', true);
