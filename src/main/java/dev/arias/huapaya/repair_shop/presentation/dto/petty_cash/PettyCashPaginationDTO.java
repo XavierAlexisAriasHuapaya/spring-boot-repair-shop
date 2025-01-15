ca79599ba3ca6a00fd116b624e92a538d66f54e8
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2025-01-15 18:58:17 -0500

    fix: pagination petty cash

--- src/main/java/dev/arias/huapaya/repair_shop/presentation/dto/petty_cash/PettyCashPaginationDTO.java
+++ src/main/java/dev/arias/huapaya/repair_shop/presentation/dto/petty_cash/PettyCashPaginationDTO.java
@@ -2,9 +2,7 @@ package dev.arias.huapaya.repair_shop.presentation.dto.petty_cash;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-import dev.arias.huapaya.repair_shop.persistence.entity.BankBoxEntity;
 import dev.arias.huapaya.repair_shop.persistence.entity.PettyCashEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,7 +15,7 @@ public class PettyCashPaginationDTO {
 
     private Long id;
 
-    private BankBoxEntity bankBox;
+    private String bankBox;
 
     private LocalDate openingDate;
 
@@ -31,20 +29,17 @@ public class PettyCashPaginationDTO {
 
     private String statusPettyCash;
 
-    private LocalDateTime createdAt;
-
     private Boolean status;
 
     public PettyCashPaginationDTO(PettyCashEntity pettyCash) {
         this.id = pettyCash.getId();
-        this.bankBox = pettyCash.getBankBox();
+        this.bankBox = pettyCash.getBankBox().getName();
         this.openingDate = pettyCash.getOpeningDate();
         this.closingDate = pettyCash.getClosingDate();
         this.openingAmount = pettyCash.getOpeningAmount();
         this.cashClosing = pettyCash.getCashClosing();
         this.otherClosing = pettyCash.getOtherClosing();
         this.statusPettyCash = pettyCash.getOpenPettyCash() ? "OPENING" : "CLOSING";
-        this.createdAt = pettyCash.getCreatedAt();
         this.status = pettyCash.getStatus();
     }
 
