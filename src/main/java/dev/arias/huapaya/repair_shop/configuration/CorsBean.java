cfa74ea8ae9a23a6f4834eee9663b17338768d0a
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2025-02-26 20:44:12 -0500

    feat: cors

---
+++ src/main/java/dev/arias/huapaya/repair_shop/configuration/CorsBean.java
@@ -0,0 +1,27 @@
+package dev.arias.huapaya.repair_shop.configuration;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsBean {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
